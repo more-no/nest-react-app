227ad6e23d6ddb9084375efb9be9c8274dbac391
Moreno Dore <moreno.dore@tuta.io>
2024-01-07 17:19:50 +0100

    Switch frontend to Angular (no standalone)

--- README.md
+++ README.md
@@ -1,13 +1,13 @@
-## Running the app:
+# Running the app
 
-### pnpm run dev:
+## pnpm run dev
 
 - backend port 5000
 
 - frontend port 5173
 
-### pnpm run build:
+## pnpm run build
 
-### pnpm run start:
+## pnpm run start
 
 - port 5000
--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -7,7 +7,6 @@ async function bootstrap() {
   // here we simply start up our HTTP listener, which lets the application await inbound HTTP requests.
   const app = await NestFactory.create(AppModule);
   // i set a GlobalPrefix that is identical to the one in 'vite.config.ts' for the path of the backend server
-  app.setGlobalPrefix('api');
-  await app.listen(5000);
+  app.listen(3000);
 }
 bootstrap();
