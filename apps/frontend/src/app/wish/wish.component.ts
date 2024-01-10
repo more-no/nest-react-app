d607f86ae82b2c3fc9f11b7e14c5eec2b3528e2f
Moreno Dore <moreno.dore@tuta.io>
2024-01-10 12:05:50 +0100

    Exporting Single Components

---
+++ apps/frontend/src/app/wish/wish.component.ts
@@ -0,0 +1,40 @@
+// created with 'ng g component wish -m wish'
+// so to create the component inside the existing wish folder
+import { Component } from '@angular/core';
+//    moved from 'app.component.ts'
+import { WishItem } from '../../shared/models/wishItem';
+import { EventService } from '../../shared/services/EventService';
+import { WishService } from './../wish/wish.service';
+
+@Component({
+  selector: 'app-wish',
+  templateUrl: './wish.component.html',
+  styleUrl: './wish.component.sass',
+})
+export class WishComponent {
+  // we use the bang so we can not initialize it
+  items: WishItem[] = [];
+  constructor(
+    event: EventService,
+    private wishService: WishService,
+  ) {
+    event.listen('removeWish', (wish: any) => {
+      let index = this.items.indexOf(wish);
+      this.items.splice(index, 1);
+    });
+  }
+  ngOnInit(): void {
+    // here we can call the service we created getWishes
+    // the get request returns an Observable, to which we must subscribe
+    this.wishService.getWishes().subscribe(
+      (data: any) => {
+        this.items = data;
+      },
+      (error: any) => {
+        alert(error.message);
+      },
+    );
+  }
+
+  filter: any;
+}
--- apps/frontend/src/app/wish/wish.module.ts
+++ apps/frontend/src/app/wish/wish.module.ts
@@ -10,6 +10,7 @@ import { WishListComponent } from './wish-list/wish-list.component';
 import { AddWishFormComponent } from './add-wish-form/add-wish-form.component';
 import { WishFilterComponent } from './wish-filter/wish-filter.component';
 import { WishListItemComponent } from './wish-list-item/wish-list-item.component';
+import { WishComponent } from './wish.component';
 
 @NgModule({
   declarations: [
@@ -18,16 +19,19 @@ import { WishListItemComponent } from './wish-list-item/wish-list-item.component
     AddWishFormComponent,
     WishFilterComponent,
     WishListItemComponent,
+    WishComponent,
   ],
   //    also "FormsModule, HttpClientModule" are moved from app.module
   imports: [CommonModule, FormsModule, HttpClientModule],
 
   //    the difference with the app.module is that we also need to export these components, so that they may be reachable by the app.component
   exports: [
-    WishListComponent,
-    AddWishFormComponent,
-    WishFilterComponent,
-    WishListItemComponent,
+    // WishListComponent,
+    // AddWishFormComponent,
+    // WishFilterComponent,
+    // WishListItemComponent,
+    //      now we need to only export the WishComponent !!
+    WishComponent,
   ],
 })
 export class WishModule {}
