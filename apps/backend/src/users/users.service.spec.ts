f0c89d6d4d3cb34d702a12e391aaddae811d6482
Moreno Dore <moreno.dore@tuta.io>
2024-02-19 19:16:16 +0100

    Add successful test for userRemove

--- apps/backend/src/users/users.service.spec.ts
+++ apps/backend/src/users/users.service.spec.ts
@@ -3,11 +3,12 @@ import { UsersService } from './users.service';
 import { PrismaService } from '../../prisma/prisma.service';
 import { UpdateUserInput } from 'src/graphql';
 import { JwtService } from '@nestjs/jwt';
-import { BadRequestException, NotFoundException } from '@nestjs/common';
+import { NotFoundException } from '@nestjs/common';
 
 describe('UsersService', () => {
   let service: UsersService;
   let prisma: PrismaService;
+  let jwtService: JwtService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -15,10 +16,21 @@ describe('UsersService', () => {
         UsersService,
         JwtService,
         {
+          provide: JwtService,
+          useValue: {
+            decode: jest.fn(),
+          },
+        },
+        {
           provide: PrismaService,
           useValue: {
             user: {
               update: jest.fn(),
+              deleteMany: jest.fn(),
+            },
+            session: {
+              findFirst: jest.fn(),
+              deleteMany: jest.fn(),
             },
           },
         },
@@ -27,6 +39,7 @@ describe('UsersService', () => {
 
     service = module.get<UsersService>(UsersService);
     prisma = module.get<PrismaService>(PrismaService);
+    jwtService = module.get<JwtService>(JwtService);
   });
 
   it('should update user info', async () => {
@@ -51,7 +64,7 @@ describe('UsersService', () => {
   });
 
   it('should throw NotFoundException if an error occurs', async () => {
-    const id = 1;
+    const id = 3;
     const dto: UpdateUserInput = {
       username: 'new Username',
       fullname: 'new Fullname',
@@ -69,5 +82,30 @@ describe('UsersService', () => {
     (prisma.user.update as jest.Mock).mockRejectedValue(error);
 
     await expect(service.update(id, dto)).rejects.toThrow(NotFoundException);
+  });
+
+  it('should delete an user and its session', async () => {
+    const userId = 3;
+    const accessToken = 'fakeAccessToken';
+    const tokenSub = 'tokenSubject';
+
+    // Mock Prisma session.findFirst to return a valid session
+    jest.spyOn(prisma.session, 'findFirst').mockResolvedValue({
+      id: 3,
+      token: 'fakeAccessToken',
+      expiryTimestamp: new Date(),
+      user_id: userId,
+    });
+
+    // Mock JWTService decode to return the token subject
+    jest.spyOn(jwtService, 'decode').mockReturnValue({ sub: tokenSub });
+
+    // Mock Prisma user.deleteMany and session.deleteMany to succeed
+    jest.spyOn(prisma.user, 'deleteMany').mockResolvedValue({ count: 1 });
+    jest.spyOn(prisma.session, 'deleteMany').mockResolvedValue({ count: 1 });
+
+    const result = await service.userRemove(userId, accessToken);
+
+    expect(result).toBe(true);
   });
 });
